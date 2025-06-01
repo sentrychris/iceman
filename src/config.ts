4f80b65cbec92cb439de0e7ec857087bcc90379c
Your Name <you@example.com>
2025-06-01 22:16:46 +0100

    examples

--- src/config.ts
+++ src/config.ts
@@ -21,8 +21,6 @@ const DISCORD_PREFIX = <string>process.env.DISCORD_PREFIX ?? '!wf';
 const FOUNDING_WARLORD_USER_ID = <string>process.env.FOUNDING_WARLORD_USER_ID; // shikaricm
 const GENERAL_WARFRAME_CHAT_CHANNEL = <string>process.env.GENERAL_WARFRAME_CHAT_CHANNEL; // #general-warframe-chat
 const CLAN_ANNOUNCEMENTS_CHANNEL = <string>process.env.CLAN_ANNOUNCEMENTS_CHANNEL;       // #nihil-vox-announcements
-const WORLD_CYCLE_TRACKING_CHANNEL  = <string>process.env.WORLD_CYCLE_TRACKING_CHANNEL;  // #world-cycle-tracking
-const WORLD_CYCLE_UPDATE_INTERVAL_MS = 60 * 60 * 1000;
 
 export {
   client,
@@ -33,7 +31,5 @@ export {
   FOUNDING_WARLORD_USER_ID,
   GENERAL_WARFRAME_CHAT_CHANNEL,
   CLAN_ANNOUNCEMENTS_CHANNEL,
-  WORLD_CYCLE_TRACKING_CHANNEL,
-  WORLD_CYCLE_UPDATE_INTERVAL_MS
 };
  
