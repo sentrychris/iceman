51c7da41aab1348f893bf28cc9b2cab9fca3d076
Your Name <you@example.com>
2025-06-02 15:45:42 +0100

    Add env

--- environment.d.ts
+++ environment.d.ts
@@ -1,18 +1,18 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            NODE_ENV: 'development' | 'production';
-            WARFRAME_API: string;
-            WARFRAME_MARKET_API: string;
-            CLAN_ICON: string;
-            DISCORD_BOT_NAME: string;
-            DISCORD_PREFIX: string;
-            DISCORD_AUTH_TOKEN: string;
-            FOUNDING_WARLORD_USER_ID: string;
-            GENERAL_WARFRAME_CHAT_CHANNEL: string;
-            CLAN_ANNOUNCEMENTS_CHANNEL: string;
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production';
+      WARFRAME_API: string;
+      WARFRAME_MARKET_API: string;
+      CLAN_ICON: string;
+      DISCORD_BOT_NAME: string;
+      DISCORD_PREFIX: string;
+      DISCORD_AUTH_TOKEN: string;
+      FOUNDING_WARLORD_USER_ID: string;
+      GENERAL_WARFRAME_CHAT_CHANNEL: string;
+      CLAN_ANNOUNCEMENTS_CHANNEL: string;
     }
+  }
 }
 
 export {}
